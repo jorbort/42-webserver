def3628d32274753808c0152f2c33017af2f0ea6
Jorge Bortolotti Suarez <jbortolo@car10s3.42barcelona.com>
2024-04-29 15:40:37 +0200

    empeazndo a mirar como organizar y probando makefile

--- Includes/WebServ.hpp
+++ Includes/WebServ.hpp
@@ -5,3 +5,16 @@
 #define YELLOW "\033[33m"
 #define CYAN "\033[106m"
 #define RESET "\033[0m"
+
+#include <sys/socket.h>
+#include <poll.h>
+#include <string>
+
+class Server
+{
+	private:
+		int sfd;
+	public:
+	void CreateSocket();
+	static void ParseConfig(std::string &arg);
+};
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,9 +1,19 @@
 #include "../Includes/Logger.hpp"
 #include "../Includes/WebServ.hpp"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	Logger::print("Error", "hay un error");
+	if (argc > 2)
+	{
+		Logger::print("Error", "Error : invalid amount of arguments");
+		return (1);
+	}
+	Server webServer;
+	if (argc == 2)
+	{
+		std::string arg = argv[1];
+		webServer.ParseConfig(arg);
+	}
 	Logger::print("Warning", "una advertencia");
 	Logger::print("Ok", "todo ok");
 	Logger::print("otro", "una opcion que no existe");
