a4e97e0f40326ec73bd04937d22708a34c7cee32
Jorge Bortolotti Suarez <jbortolo@car3s1.42barcelona.com>
2024-05-06 14:00:46 +0200

    mas  o menos tengo el multiplexing creo, estoy en mac y no lo puedo complilar

--- Src/WebServ.cpp
+++ Src/WebServ.cpp
@@ -5,16 +5,27 @@
 #include <iostream>
 #include <unistd.h>
 
+#define MAX_EVENTS 10000
+
 void Server::RunServer(void)
 {
 	int new_socket;
+	int epoll_fd = epoll_create1(0);
+	int eventCount;
+
 	std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-	this->sfd = socket(AF_INET, SOCK_STREAM, 0);  
+	this->sfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (this->sfd == -1)
 		throw SocketException();
-	
+	if (epoll_fd < 0)
+		throw SocketException();
 	sockaddr_in port;
+
+	struct epoll_event event, events[MAX_EVENTS];
 	int portlen = sizeof(port);
+
+	event.events = EPOLLIN;
+	event.data.fd = 0;
 
 	port.sin_family = AF_INET;
 	port.sin_port = htons(4242);
@@ -29,17 +40,31 @@ void Server::RunServer(void)
 		throw SocketException();
 	while (42)
 	{
+		char buffer [30000] = {0};
+
 		std::cout << "waiting for conection" << std::endl;
 		if (( new_socket = accept(this->sfd,(struct sockaddr *) &port, (socklen_t *)&portlen)) < 0)
+			{
+				close(epoll_fd);
+				throw SocketException();
+			}
+		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_socket, &event))
+		{
+			close(epoll_fd);
 			throw SocketException();
-		char buffer [3000] = {0};
-		if (read(new_socket, buffer, 3000) < 0)
-			throw SocketException();
-		std::cout << buffer << std::endl;
-		write(new_socket, response.c_str(), response.length());
-		close(new_socket);
-	}
+		}
+		eventCount = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
+		for(int i = 0; i < eventCount; i++)
+		{
+			if (read(events[i].data.fd, buffer, 30000) < 0)
+				throw SocketException();
+			std::cout << buffer << std::endl;
+			write(new_socket, response.c_str(), response.length());
 
+		}
+		// close(new_socket);
+	}
+	close(epoll_fd);
 }
 
 const char * Server::SocketException::what() const throw()
