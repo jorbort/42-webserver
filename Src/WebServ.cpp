494027b8d207622e3b9ccf8dfe918cf438e5a760
jorbort <jorbort92@gmail.com>
2024-05-08 17:18:06 +0200

    un poco de liempieza en la funcion de crear el servidor

--- Src/WebServ.cpp
+++ Src/WebServ.cpp
@@ -9,66 +9,64 @@
 
 #define MAX_EVENTS 10000
 
+int makeSocket(sockaddr_in &port, int portlen)
+{
+	int n =  socket(AF_INET, SOCK_STREAM, 0);
+	port.sin_family = AF_INET;
+	port.sin_port = htons(4242);
+	port.sin_addr.s_addr = INADDR_ANY;
+	memset(port.sin_zero, '\0', sizeof(port.sin_zero));
+	if (bind(n, (struct sockaddr *) &port, sizeof(port)) == -1)
+		return -1;
+	if (listen(n, 10) < 0)
+		return -1;
+
+}
+
 void Server::RunServer(void)
 {
 	int new_socket;
 	int epoll_fd = epoll_create1(0);
 	int eventCount;
+	sockaddr_in port;
+	int portlen = sizeof(port);
 
 	std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-	this->sfd = socket(AF_INET, SOCK_STREAM, 0);
+	this->sfd = makeSocket(port, portlen);
 	if (this->sfd == -1)
 		throw SocketException();
 	if (epoll_fd < 0)
 		throw SocketException();
-	sockaddr_in port;
 
 	struct epoll_event event, events[MAX_EVENTS];
-	int portlen = sizeof(port);
 
 	event.events = EPOLLIN;
 	event.data.fd = this->sfd;
 
-	port.sin_family = AF_INET;
-	port.sin_port = htons(4242);
-	port.sin_addr.s_addr = INADDR_ANY;
-
-	memset(port.sin_zero, '\0', sizeof(port.sin_zero));
-
-	if (bind(this->sfd, (struct sockaddr *) &port, sizeof(port)) == -1)
-		throw SocketException();
-
-	if (listen(this->sfd, 10) < 0)
-		throw SocketException();
-	fcntl(this->sfd, F_SETFL, O_NONBLOCK);
-	
-	std::cout << "waiting for conection" << std::endl;
 	while (42)
 	{
 		char buffer [30000] = {0};
 
+		std::cout << GREEN <<  "waiting for conection..." << std::endl;
 		if (( new_socket = accept(this->sfd,(struct sockaddr *) &port, (socklen_t *)&portlen)) < 0)
 		{
-			if(errno == EAGAIN || errno == EWOULDBLOCK)
-			{
-					continue;
-			}
-			else
-			{
+			// if(errno == EAGAIN || errno == EWOULDBLOCK)
+			// {
+			// 		continue;
+			// }
+			// else
+			// {
 				close(epoll_fd);
 				throw SocketException();
-			}
+			// }
 		}
 		event.data.fd = new_socket;
-		std::cout << new_socket << std::endl;
 		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_socket, &event))
 		{
 			close(epoll_fd);
 			throw SocketException();
 		}
-		std::cout << "pasa epollctl" <<std::endl;
 		eventCount = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
-		std::cout << eventCount;
 		for(int i = 0; i < eventCount; i++)
 		{
 			if (read(events[i].data.fd, buffer, 30000) < 0)
@@ -77,7 +75,6 @@ void Server::RunServer(void)
 			write(new_socket, response.c_str(), response.length());
 
 		}
-		// close(new_socket);
 	}
 	close(epoll_fd);
 }
