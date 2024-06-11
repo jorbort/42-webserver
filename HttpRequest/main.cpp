7c867ae74948b6f271c4541f0e9c806b142d2754
Juan Antonio <juanantoniomartinezmorales@Juans-Laptop.local>
2024-06-11 17:51:18 +0200

    HttpRequest parser 90% falta testar por compis

--- HttpRequest/main.cpp
+++ HttpRequest/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juan-anm  <juan-anm@student.42barcel>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 18:38:04 by juan-anm          #+#    #+#             */
-/*   Updated: 2024/06/11 17:37:34 by juan-anm         ###   ########.fr       */
+/*   Updated: 2024/06/11 17:50:29 by juan-anm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,44 @@
 // Content-Length: 36
 // "Hello World" in a URI would be encoded as Hello%20World.
 
+// GET /index.html HTTP/1.1\r\n
+// Host: www.example.com\r\n
+// User-Agent: curl/7.64.1\r\n
+// Accept: */*\r\n
+// \r\n
+
+// "\r\n"
+// 						// "POST api.example.com:80.7/docs/web/error_pages?Name=Juan&Bar=Manolo HTTP/1.1\r\n"
+// 						"POST /docs/web/error_pages?Name=Juan&Bar=Manolo HTTP/1.1\r\n"
+// 						"Host: 		api.example.com\r\n"
+// 						"Content-Type: application/json				\r\n"
+// 						"Authorization: Bearer YOUR_ACCESS_TOKEN\r\n"
+// 						"Content-Length: 36\r\n"
+// 						"Transfer-Encoding: chunked\r\n"
+// 						"\r\n"
+// 						"{\"key1\"\0: \"value1\", \"key2\": \"value2\"}"
+
+// POST /submit HTTP/1.1\r\n
+// Host: www.example.com\r\n
+// Content-Type: application/x-www-form-urlencoded\r\n
+// Content-Length: 27\r\n
+// \r\n
+// field1=value1&field2=value2
+
 int main(void)
 {
 	// const char* Request = "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
 	
 	char Request2[30000] = 	
-						{"\r\n"
-						// "POST api.example.com:80.7/docs/web/error_pages?Name=Juan&Bar=Manolo HTTP/1.1\r\n"
-						"POST /docs/web/error_pages?Name=Juan&Bar=Manolo HTTP/1.1\r\n"
-						"Host: 		api.example.com\r\n"
-						"Content-Type: application/json				\r\n"
-						"Authorization: Bearer YOUR_ACCESS_TOKEN\r\n"
-						"Content-Length: 36\r\n"
-						"Transfer-Encoding: chunked\r\n"
-						"\r\n"
-						"{\"key1\"\0: \"value1\", \"key2\": \"value2\"}"};
+						{
+							"POST /submit HTTP/1.1\r\n"
+							"Host: www.example.com\r\n"
+							"Content-Type: application/x-www-form-urlencoded\r\n"
+							"Content-Length: 27\r\n"
+							"\r\n"
+							"field1=value1&field2=value2"
+
+						};
 
 	HttpRequest request;
 	HttpRequestParser Request_parser;
