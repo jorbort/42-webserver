d66f1aa2da579aaed8e402160c158aed2221cb87
Juan Antonio <juanantoniomartinezmorales@Juans-Laptop.local>
2024-06-11 16:55:00 +0200

    UrI parser update 85%

--- HttpRequest/HTTPRequest.cpp
+++ HttpRequest/HTTPRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: juan-anm  <juan-anm@student.42barcel>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 00:28:57 by juan-anm          #+#    #+#             */
-/*   Updated: 2024/06/06 17:54:54 by juan-anm         ###   ########.fr       */
+/*   Updated: 2024/06/10 19:20:16 by juan-anm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,19 +67,31 @@ void	HttpRequest::setBody(const std::vector<uint8_t> &body){
 
 std::ostream& operator<<(std::ostream &out, const HttpRequest &request){
 	std::map<std::string, std::string> headers = request.getHeaders();
+	std::map<std::string, std::string> Query = request._URI_query;
 	std::vector<uint8_t> body = request.getBody();
-
+	
+	out << std::endl;
 	out << "Method: " << request.getMethod() << std::endl;
+	out << std::endl;
 	out << "URI: " << request.getURI() << std::endl;
+	out << "URI Path: " << request._URI_path << std::endl;
+	out << "URI Port: " << request._URI_tcpPort << std::endl;
+	for (std::map<std::string, std::string>::iterator it = Query.begin(); it != Query.end(); it++){
+		out << it->first << ": " << it->second << std::endl;
+	}
+	out << std::endl;
 	out << "Version: " << request.getVersion() << std::endl;
+	out << std::endl;
 	out << "Headers: " << std::endl;
 	for (std::map<std::string, std::string>::reverse_iterator it = headers.rbegin(); it != headers.rend(); it++){
 		out << it->first << ": " << it->second << std::endl;
 	}
+	out << std::endl;
 	out << "Body: " << std::endl;
 	for (std::vector<uint8_t>::iterator it = body.begin(); it != body.end(); it++){
 		out << *it;
 	}
+	out << std::endl;
 	out << std::endl << "Error Code: " << request.getErrorCode() << std::endl;
 	return (out);
 }
